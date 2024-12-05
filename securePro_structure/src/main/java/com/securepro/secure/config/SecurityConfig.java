5c6e146cb2bc6a1abd2bb0324230463c19517b45
MesVortex <meskinemostafa4@gmail.com>
2024-12-05 09:34:27 +0100

    Change from oracleXE to mariaDB as the project's database

--- securePro_structure/src/main/java/com/securepro/secure/config/SecurityConfig.java
+++ securePro_structure/src/main/java/com/securepro/secure/config/SecurityConfig.java
@@ -1,21 +1,21 @@
 package com.securepro.secure.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import javax.sql.DataSource;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//import javax.sql.DataSource;
 
-@Configuration
+//@Configuration
 public class SecurityConfig {
 
-    private final DataSource dataSource;
-
-    public SecurityConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+//    private final DataSource dataSource;
+//
+//    public SecurityConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
 
 //    @Bean
 //    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -31,18 +31,18 @@ public class SecurityConfig {
 //                .logout().permitAll();
 //        return http.build();
 //    }
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public void configureAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication()
-                .dataSource(dataSource)
-                .usersByUsernameQuery("SELECT login, password, active FROM user WHERE login = ?")
-                .authoritiesByUsernameQuery("SELECT u.login, r.name FROM user u INNER JOIN role r ON u.id = r.user_id WHERE u.login = ?")
-                .passwordEncoder(passwordEncoder());
-    }
+//
+//    @Bean
+//    public BCryptPasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public void configureAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.jdbcAuthentication()
+//                .dataSource(dataSource)
+//                .usersByUsernameQuery("SELECT login, password, active FROM user WHERE login = ?")
+//                .authoritiesByUsernameQuery("SELECT u.login, r.name FROM user u INNER JOIN role r ON u.id = r.user_id WHERE u.login = ?")
+//                .passwordEncoder(passwordEncoder());
+//    }
 }
--- securePro_structure/src/test/java/com/securepro/secure/SecureApplicationTests.java
+++ securePro_structure/src/test/java/com/securepro/secure/SecureApplicationTests.java
@@ -1,13 +1,13 @@
-package com.securepro.secure;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SecureApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.securepro.secure;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SecureApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
